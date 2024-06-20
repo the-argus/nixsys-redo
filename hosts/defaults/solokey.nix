2f65b608f715a4e1f0b2d2fea6971bd8fbcd6242
Ian McFarlane <i.mcfarlane2002@gmail.com>
2024-06-20 01:22:35 -0500

    fix emptty by fixing solokey stuff related to pam...

--- hosts/defaults/solokey.nix
+++ hosts/defaults/solokey.nix
@@ -13,13 +13,13 @@
 
     LABEL="solokeys_end"'';
 
+  # line that might be necessary to add:
+  # @include common-auth
   security.pam.u2f.enable = true;
-
-  security.pam.services.login.text = lib.mkBefore ''
+  security.pam.services.login.text = lib.mkDefault (lib.mkBefore ''
     auth sufficient pam_u2f.so
-  '';
-
-  security.pam.services.sudo.text = pkgs.lib.mkBefore ''
+  '');
+  security.pam.services.sudo.text = pkgs.lib.mkDefault (pkgs.lib.mkBefore ''
     auth sufficient pam_u2f.so
-  '';
+  '');
 }
